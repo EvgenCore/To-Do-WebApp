307acdc4ab105030918cab958c4351510d676bc8
Evgenii Igoshin <johnshin378@gmail.com>
2024-04-23 18:43:19 +0200

    0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./app/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'verySoftOrange': '#edc7b7',
+        'darkModeratePink': '#AC3B61',
+        'lightGrayishOrange': '#eee2dc',
+        'veryDarkBlue': '#123c69',
+        'customGreen': '#607e2d',
+      },
+    },
   },
   plugins: [],
 }
